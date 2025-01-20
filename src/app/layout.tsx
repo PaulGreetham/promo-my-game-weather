374d8a1a71a96d000426192ea760e50a7670a3b1
Paul Greetham <pgreetham@protonmail.com>
2025-01-20 14:41:04 +0100

    style changes to background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,32 +23,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${bebasNeue.variable} antialiased min-h-screen`}>
+      <body className={`${bebasNeue.variable} antialiased min-h-screen bg-[#004700] dark:bg-[#111111]`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {/* Fixed background */}
-          <div className="fixed inset-0 w-full h-full bg-[#006600] dark:bg-[#111111]">
+          {/* Fixed navbar */}
+          <Navbar />
+          
+          {/* Background with dots */}
+          <div className="fixed inset-x-0 top-[72px] bottom-0 z-[1]">
             <DotPattern 
               width={32} 
               height={32} 
               cx={2} 
               cy={2} 
               cr={1} 
-              className="opacity-20 fill-white [mask-image:radial-gradient(50%_50%_at_center,white,transparent)] sm:[mask-image:radial-gradient(70%_70%_at_center,white,transparent)] md:[mask-image:radial-gradient(100%_100%_at_center,white,transparent)]"
+              className="opacity-20 fill-white [mask-image:radial-gradient(65%_65%_at_center,white,transparent)] sm:[mask-image:radial-gradient(70%_70%_at_center,white,transparent)] md:[mask-image:radial-gradient(100%_100%_at_center,white,transparent)]"
             />
           </div>
           
-          {/* Fixed navbar */}
-          <div className="fixed top-0 left-0 right-0 z-[3]">
-            <Navbar />
-          </div>
-          
           {/* Main content */}
-          <main className="relative z-[2] min-h-screen">
+          <main className="relative z-[2] min-h-screen pt-[72px]">
             {children}
           </main>
         </ThemeProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "./mode-toggle"
 
 export function Navbar() {
   return (
-    <div className="w-full flex justify-between items-center bg-[#006600] dark:bg-[#111111]">
+    <nav className="w-full flex justify-between items-center bg-[#004700] dark:bg-[#111111] fixed top-0 left-0 right-0 z-[3] h-[72px]">
       <div className="text-4xl text-white font-[family-name:var(--font-bebas-neue)] p-4">
         My Game Weather
       </div>
@@ -13,6 +13,6 @@ export function Navbar() {
         <ModeToggle />
         <HamburgerMenu />
       </div>
-    </div>
+    </nav>
   )
 } 
